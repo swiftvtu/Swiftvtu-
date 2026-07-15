7da2816ec3d5211de434cc9d03975392fa8aeb12
swiftvtu <villaztips@gmail.com>
2026-07-15 19:48:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from slowapi.errors import RateLimitExceeded
